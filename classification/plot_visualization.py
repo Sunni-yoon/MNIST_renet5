52ac9e5a5ff75c03267eef44d34bafcce2d6e85e
Sunny <118954283+seonyoung0@users.noreply.github.com>
2024-04-16 23:48:44 +0900

    Update plot_visualization.py

--- classification/plot_visualization.py
+++ classification/plot_visualization.py
@@ -3,28 +3,28 @@ import matplotlib.pyplot as plt
 def visualization(trn_loss_avg, trn_acc_avg, tst_loss_avg, tst_acc_avg):
     fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 10))
 
-    # draw train loss graph
+    # train loss graph
     axes[0, 0].plot(trn_loss_avg[0], label='LeNet-5', c='b')
     axes[0, 0].plot(trn_loss_avg[1], label='custom', c='g')
     axes[0, 0].plot(trn_loss_avg[2], label='regularized LeNet-5', c='r')
     axes[0, 0].set_title('Train Loss')
     axes[0, 0].legend()
 
-    # draw train acc graph
+    # train acc graph
     axes[0, 1].plot(trn_acc_avg[0], label='LeNet-5', c='b')
     axes[0, 1].plot(trn_acc_avg[1], label='custom', c='g')
     axes[0, 1].plot(trn_acc_avg[2], label='regularized LeNet-5', c='r')
     axes[0, 1].set_title('Train Accuracy')
     axes[0, 1].legend()
 
-    # draw validation loss graph
+    # validation loss graph
     axes[1, 0].plot(tst_loss_avg[0], label='LeNet-5', c='b')
     axes[1, 0].plot(tst_loss_avg[1], label='custom', c='g')
     axes[1, 0].plot(tst_loss_avg[2], label='regularized LeNet-5', c='r')
     axes[1, 0].set_title('Validation Loss')
     axes[1, 0].legend()
 
-    # draw validation acc graph
+    # validation acc graph
     axes[1, 1].plot(tst_acc_avg[0], label='LeNet-5', c='b')
     axes[1, 1].plot(tst_acc_avg[1], label='custom', c='g')
     axes[1, 1].plot(tst_acc_avg[2], label='regularized LeNet-5', c='r')
@@ -34,5 +34,6 @@ def visualization(trn_loss_avg, trn_acc_avg, tst_loss_avg, tst_acc_avg):
     plt.savefig('result.png')
 
     plt.show()
-    
+
+# trn_loss_avg, trn_acc_avg, tst_loss_avg, tst_acc_avg는 main.py에 있는 데이터를 사용
 visualization(trn_loss_avg, trn_acc_avg, tst_loss_avg, tst_acc_avg)
