e1344bab27a8d0dba80301a9ee3e093bb824cc3f
Sunny <118954283+seonyoung0@users.noreply.github.com>
2024-04-16 23:39:29 +0900

    Update main.py

--- classification/main.py
+++ classification/main.py
@@ -108,40 +108,6 @@ def test(model, tst_loader, device, criterion):
     return tst_loss, tst_loss_list, acc, tst_acc_list
 
 
-def visualization(trn_loss_avg, trn_acc_avg, tst_loss_avg, tst_acc_avg):
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 10))
-
-    # draw train loss graph
-    axes[0, 0].plot([x.cpu().numpy() for x in trn_loss_avg[0]], label='LeNet-5', c='b')
-    axes[0, 0].plot([x.cpu().numpy() for x in trn_loss_avg[1]], label='regularized LeNet-5', c='g')
-    axes[0, 0].plot([x.cpu().numpy() for x in trn_loss_avg[2]], label='custom', c='r')
-    axes[0, 0].set_title('Train Loss')
-    axes[0, 0].legend()
-
-    # draw train acc graph
-    axes[0, 1].plot([x.cpu().numpy() for x in trn_acc_avg[0]], label='LeNet-5', c='b')
-    axes[0, 1].plot([x.cpu().numpy() for x in trn_acc_avg[1]], label='regularized LeNet-5', c='g')
-    axes[0, 1].plot([x.cpu().numpy() for x in trn_acc_avg[2]], label='custom', c='r')
-    axes[0, 1].set_title('Train Accuracy')
-    axes[0, 1].legend()
-
-    # draw validation loss graph
-    axes[1, 0].plot([x.cpu().numpy() for x in tst_loss_avg[0]], label='LeNet-5', c='b')
-    axes[1, 0].plot([x.cpu().numpy() for x in tst_loss_avg[1]], label='regularized LeNet-5', c='g')
-    axes[1, 0].plot([x.cpu().numpy() for x in tst_loss_avg[2]], label='custom', c='r')
-    axes[1, 0].set_title('Validation Loss')
-    axes[1, 0].legend()
-
-    # draw validation acc graph
-    axes[1, 1].plot([x.cpu().numpy() for x in tst_acc_avg[0]], label='LeNet-5', c='b')
-    axes[1, 1].plot([x.cpu().numpy() for x in tst_acc_avg[1]], label='regularized LeNet-5', c='g')
-    axes[1, 1].plot([x.cpu().numpy() for x in tst_acc_avg[2]], label='custom', c='r')
-    axes[1, 1].set_title('Validation Accuracy')
-    axes[1, 1].legend()
-
-    plt.savefig('result.png')
-
-
 def main():
     """ Main function
 
@@ -319,9 +285,3 @@ def main():
 
     print(tst_loss_avg)
     print(tst_acc_avg)
-
-    visualization(trn_loss_avg, trn_acc_avg, tst_loss_avg, tst_acc_avg)
-
-
-if __name__ == '__main__':
-    main()
